2df3655065f2e4a46f16c77ff22a273dfa5595c8
Ulrik <ulrik@netpower.no>
2023-04-27 14:01:54 +0200

    next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    OPENAI_API_KEY: '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',
+    NEXT_PUBLIC_OPENAI_API_KEY:
+      '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',
+    NEXT_PUBLIC_SUPABASE_URL: 'https://fwiyhnwcrlydinggswoe.supabase.co',
+    SUPABASE_SERVICE_ROLE_KEY:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ3aXlobndjcmx5ZGluZ2dzd29lIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODIwOTIxNTksImV4cCI6MTk5NzY2ODE1OX0.LkbVrfkJe7yTxt0t6RS_ciiizXBSQ6JcNeYjhDdAjoE',
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
