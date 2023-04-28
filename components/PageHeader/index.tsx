c8c603960d2301fecc40be8b705545ad9eb3c109
Ulrik Håland <ulrik@netpower.no>
2023-04-28 22:03:33 +0200

    prompt eng

--- components/PageHeader/index.tsx
+++ components/PageHeader/index.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import React, { useEffect, useState } from "react";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import {
   IconButton,
   Dialog,
@@ -8,23 +8,15 @@ import {
   DialogContentText,
   DialogActions,
   Button,
-} from '@mui/material';
-
-interface User {
-  ageGroup?: string;
-  gender?: string;
-  activityLevel?: string;
-  dietaryPreferences?: string;
-  healthGoals?: string;
-  allergies?: string;
-  sleepHabits?: string;
-  stressLevels?: string;
-  timeAvailability?: string;
-}
+} from "@mui/material";
+import { User } from "@/stores/general.store";
+import { useStore } from "../../stores/RootStoreProvider";
 
 const PageHeader = () => {
+  const { generalStore } = useStore();
+
   const [open, setOpen] = useState(false);
-  const [user, setUser] = useState<User>({});
+  const [user, setUser] = useState<User>(generalStore.user);
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -45,18 +37,30 @@ const PageHeader = () => {
     e.preventDefault();
     console.log(user);
     setOpen(false);
+    localStorage.setItem("user", JSON.stringify(user));
   };
 
+  useEffect(() => {
+    generalStore.setUser(user);
+  }, [generalStore, user]);
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
+
   return (
-    <div className='border-b w-full'>
-      <div className='grid grid-cols-3 items-center'>
+    <div className="border-b w-full">
+      <div className="grid grid-cols-3 items-center">
         <div></div>
-        <div className='text-center'>
-          <h2 className='text-2xl font-bold mt-4 mb-2'>ChatLongevity</h2>
-          <h2 className='text-m mb-2 text-gray-600'>Model: Chattia01</h2>
+        <div className="text-center">
+          <h2 className="text-2xl font-bold mt-4 mb-2">ChatLongevity</h2>
+          <h2 className="text-m mb-2 text-gray-600">Model: Chattia01</h2>
         </div>
-        <div className='text-right mr-4'>
-          <IconButton color='inherit' onClick={handleClickOpen}>
+        <div className="text-right mr-4">
+          <IconButton color="inherit" onClick={handleClickOpen}>
             <AccountCircleIcon />
           </IconButton>
         </div>
@@ -64,22 +68,23 @@ const PageHeader = () => {
       <Dialog
         open={open}
         onClose={handleClose}
-        aria-labelledby='form-dialog-title'
+        aria-labelledby="form-dialog-title"
       >
-        <DialogTitle id='form-dialog-title'>User Information</DialogTitle>
+        <DialogTitle id="form-dialog-title">User Information</DialogTitle>
         <DialogContent>
-          <form className='space-y-4'>
-            <div className='flex flex-col'>
+          <form className="space-y-4">
+            <div className="flex flex-col">
               <label
-                htmlFor='age-group'
-                className='text-sm font-medium text-gray-700'
+                htmlFor="age-group"
+                className="text-sm font-medium text-gray-700"
               >
                 Age Group
               </label>
               <select
                 onChange={handleChange}
-                id='age-group'
-                className='rounded border border-gray-300 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 text-base outline-none text-gray-700 py-1 px-3'
+                id="ageGroup"
+                className="rounded border border-gray-300 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 text-base outline-none text-gray-700 py-1 px-3"
+                value={user.ageGroup || ""}
               >
                 <option>Select age group</option>
                 <option>18-24</option>
@@ -90,17 +95,18 @@ const PageHeader = () => {
                 <option>65+</option>
               </select>
             </div>
-            <div className='flex flex-col'>
+            <div className="flex flex-col">
               <label
-                htmlFor='gender'
-                className='text-sm font-medium text-gray-700'
+                htmlFor="gender"
+                className="text-sm font-medium text-gray-700"
               >
                 Gender
               </label>
               <select
                 onChange={handleChange}
-                id='gender'
-                className='rounded border border-gray-300 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 text-base outline-none text-gray-700 py-1 px-3'
+                id="gender"
+                className="rounded border border-gray-300 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 text-base outline-none text-gray-700 py-1 px-3"
+                value={user.gender || ""}
               >
                 <option>Select gender</option>
                 <option>Male</option>
@@ -109,17 +115,18 @@ const PageHeader = () => {
                 <option>Prefer not to say</option>
               </select>
             </div>
-            <div className='flex flex-col'>
+            <div className="flex flex-col">
               <label
-                htmlFor='activity-level'
-                className='text-sm font-medium text-gray-700'
+                htmlFor="activityLevel"
+                className="text-sm font-medium text-gray-700"
               >
                 Activity Level
               </label>
               <select
                 onChange={handleChange}
-                id='activity-level'
-                className='rounded border border-gray-300 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 text-base outline-none text-gray-700 py-1 px-3'
+                id="activityLevel"
+                className="rounded border border-gray-300 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 text-base outline-none text-gray-700 py-1 px-3"
+                value={user.activityLevel || ""}
               >
                 <option>Select activity level</option>
                 <option>Sedentary</option>
@@ -128,17 +135,18 @@ const PageHeader = () => {
                 <option>Very active</option>
               </select>
             </div>
-            <div className='flex flex-col'>
+            <div className="flex flex-col">
               <label
-                htmlFor='dietary-preferences'
-                className='text-sm font-medium text-gray-700'
+                htmlFor="dietary-preferences"
+                className="text-sm font-medium text-gray-700"
               >
                 Dietary Preferences
               </label>
               <select
                 onChange={handleChange}
-                id='dietary-preferences'
-                className='rounded border border-gray-300 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 text-base outline-none text-gray-700 py-1 px-3'
+                id="dietaryPreferences"
+                className="rounded border border-gray-300 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 text-base outline-none text-gray-700 py-1 px-3"
+                value={user.dietaryPreferences || ""}
               >
                 <option>Select dietary preference</option>
                 <option>Standard</option>
@@ -149,17 +157,18 @@ const PageHeader = () => {
                 <option>Other</option>
               </select>
             </div>
-            <div className='flex flex-col'>
+            <div className="flex flex-col">
               <label
-                htmlFor='health-goals'
-                className='text-sm font-medium text-gray-700'
+                htmlFor="health-goals"
+                className="text-sm font-medium text-gray-700"
               >
                 General Health Goals
               </label>
               <select
                 onChange={handleChange}
-                id='health-goals'
-                className='rounded border border-gray-300 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 text-base outline-none text-gray-700 py-1 px-3'
+                id="healthGoals"
+                className="rounded border border-gray-300 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 text-base outline-none text-gray-700 py-1 px-3"
+                value={user.healthGoals || ""}
               >
                 <option>Select health goal</option>
                 <option>Weight loss</option>
@@ -169,47 +178,50 @@ const PageHeader = () => {
                 <option>Stress management</option>
               </select>
             </div>
-            <div className='flex flex-col'>
+            <div className="flex flex-col">
               <label
-                htmlFor='allergies'
-                className='text-sm font-medium text-gray-700'
+                htmlFor="allergies"
+                className="text-sm font-medium text-gray-700"
               >
                 Allergies or Intolerances (optional)
               </label>
               <input
                 onChange={handleChange}
-                type='text'
-                id='allergies'
-                placeholder='e.g. lactose, peanuts'
-                className='rounded border border-gray-300 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 text-base outline-none text-gray-700 py-1 px-3'
+                type="text"
+                id="allergies"
+                placeholder="e.g. lactose, peanuts"
+                className="rounded border border-gray-300 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 text-base outline-none text-gray-700 py-1 px-3"
+                value={user.allergies || ""}
               />
             </div>
-            <div className='flex flex-col'>
+            <div className="flex flex-col">
               <label
-                htmlFor='sleep-habits'
-                className='text-sm font-medium text-gray-700'
+                htmlFor="sleepHabits"
+                className="text-sm font-medium text-gray-700"
               >
                 Typical Sleep Duration
               </label>
               <input
                 onChange={handleChange}
-                type='text'
-                id='sleep-habits'
-                placeholder='e.g. 7 hours'
-                className='rounded border border-gray-300 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 text-base outline-none text-gray-700 py-1 px-3'
+                type="text"
+                id="sleepHabits"
+                placeholder="e.g. 7 hours"
+                className="rounded border border-gray-300 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 text-base outline-none text-gray-700 py-1 px-3"
+                value={user.sleepHabits || ""}
               />
             </div>
-            <div className='flex flex-col'>
+            <div className="flex flex-col">
               <label
-                htmlFor='stress-levels'
-                className='text-sm font-medium text-gray-700'
+                htmlFor="stress-levels"
+                className="text-sm font-medium text-gray-700"
               >
                 Stress Levels
               </label>
               <select
                 onChange={handleChange}
-                id='stress-levels'
-                className='rounded border border-gray-300 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 text-base outline-none text-gray-700 py-1 px-3'
+                id="stressLevels"
+                className="rounded border border-gray-300 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 text-base outline-none text-gray-700 py-1 px-3"
+                value={user.stressLevels || ""}
               >
                 <option>Select stress level</option>
                 <option>Low</option>
@@ -217,28 +229,29 @@ const PageHeader = () => {
                 <option>High</option>
               </select>
             </div>
-            <div className='flex flex-col'>
+            <div className="flex flex-col">
               <label
-                htmlFor='time-availability'
-                className='text-sm font-medium text-gray-700'
+                htmlFor="time-availability"
+                className="text-sm font-medium text-gray-700"
               >
                 Time Availability for Exercise and Meal Prep
               </label>
               <input
                 onChange={handleChange}
-                type='text'
-                id='time-availability'
-                placeholder='e.g. 1 hour per day'
-                className='rounded border border-gray-300 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 text-base outline-none text-gray-700 py-1 px-3'
+                type="text"
+                id="timeAvailability"
+                placeholder="e.g. 1 hour per day"
+                className="rounded border border-gray-300 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 text-base outline-none text-gray-700 py-1 px-3"
+                value={user.timeAvailability || ""}
               />
             </div>
-          </form>{' '}
+          </form>{" "}
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose} color='primary'>
+          <Button onClick={handleClose} color="primary">
             Close
           </Button>
-          <Button type='submit' color='primary'>
+          <Button type="submit" color="primary" onClick={handleSubmit}>
             Submit
           </Button>
         </DialogActions>
--- utils/preQuestion.ts
+++ utils/preQuestion.ts
@@ -1,22 +1,22 @@
-import endent from 'endent';
-import { openai } from './openAIConfig';
+import endent from "endent";
+import { openai } from "./openAIConfig";
 
 export const getPreQuestion = async (previousQuestion: string[]) => {
   let response;
 
   try {
     response = await openai.createChatCompletion({
-      model: 'gpt-3.5-turbo-0301',
+      model: "gpt-3.5-turbo-0301",
       messages: [
         {
-          role: 'user',
+          role: "user",
           content: endent`
-            I need a question that is health related, it should be related to exercise, nutrition, sleep, cardiovascular health, lipids, blood pressure, blood sugar, or something similar.
+            I need a question that is health related, it should be related to exercise, nutrition, sleep, alcohol, cardiovascular health, lipids, blood pressure, blood sugar, or something similar.
             The question should start with the word 'How', 'What', or 'Why'.
             The question should be specific and advanced, it should not be a general question.
             Assure that the question is not identical to any of the previous questions.
             The previous questions are delimited by triple quotes.
-            """${previousQuestion.join('\n')}"""
+            """${previousQuestion.join("\n")}"""
             The question should be something that a person who is interested in health, nutrition and exercise would ask.
             The question must be maximally 5 words long. This is very important!
             `,
@@ -27,11 +27,11 @@ export const getPreQuestion = async (previousQuestion: string[]) => {
     });
   } catch (e) {
     console.log(e);
-    throw new Error('Error in GPT-3 request');
+    throw new Error("Error in GPT-3 request");
   }
 
   if (response.status !== 200) {
-    throw new Error('OpenAI API returned an error');
+    throw new Error("OpenAI API returned an error");
   }
 
   const message = response.data.choices[0].message?.content;
