59c018d35d88c9eeca5250d654aff6d5fe723e05
Ulrik <ulrik@netpower.no>
2023-05-04 18:35:53 +0200

    scrape

--- scripts/scrape.ts
+++ scripts/scrape.ts
@@ -2,11 +2,11 @@ import puppeteer from 'puppeteer';
 import * as fs from 'fs';
 import csv from 'csv-parser';
 import { encode } from 'gpt-3-encoder';
-import { Chunk } from '@/types';
 import { createClient } from '@supabase/supabase-js';
 import { Configuration, OpenAIApi } from 'openai';
 import { loadEnvConfig } from '@next/env';
 import { JSDOM } from 'jsdom';
+import { Restaurant } from '@/types';
 
 const loginUrl = 'https://peterattiamd.com/login/';
 const targetUrl = 'https://peterattiamd.com/ama39/';
@@ -20,7 +20,7 @@ export type Scraped = {
   category: string;
   date: string;
   contentEncoded?: EncodedString[];
-  chunks?: Chunk[];
+  chunks?: Restaurant[];
 };
 
 export type EncodedString = {
@@ -37,7 +37,7 @@ const configuration = new Configuration({
 
 export const openai = new OpenAIApi(configuration);
 
-const generateEmbeddings = async (chunks: Chunk[]): Promise<void> => {
+const generateEmbeddings = async (chunks: Restaurant[]): Promise<void> => {
   const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
   });
@@ -49,46 +49,40 @@ const generateEmbeddings = async (chunks: Chunk[]): Promise<void> => {
   );
 
   for (let i = 0; i < chunks.length; i++) {
-    const chunk = chunks[i];
-
-    const {
-      title,
-      date,
-      content,
-      context,
-      people,
-      content_length,
-      content_tokens,
-    } = chunk;
-
-    const embeddingResponse = await openai.createEmbedding({
-      model: 'text-embedding-ada-002',
-      input: content,
-    });
-
-    const [{ embedding }] = embeddingResponse.data.data;
-
-    const { data, error } = await supabase
-      .from('training')
-      .insert({
-        title,
-        date,
-        context,
-        people,
-        content,
-        content_length,
-        content_tokens,
-        embedding,
-      })
-      .select('*');
-
-    if (error) {
-      console.log('error', error);
-    } else {
-      console.log('saved', i, i);
-    }
-
-    await new Promise((resolve) => setTimeout(resolve, 200));
+    // const chunk = chunks[i];
+    // const {
+    //   title,
+    //   date,
+    //   content,
+    //   context,
+    //   people,
+    //   content_length,
+    //   content_tokens,
+    // } = chunk;
+    // const embeddingResponse = await openai.createEmbedding({
+    //   model: 'text-embedding-ada-002',
+    //   input: content,
+    // });
+    // const [{ embedding }] = embeddingResponse.data.data;
+    // const { data, error } = await supabase
+    //   .from('training')
+    //   .insert({
+    //     title,
+    //     date,
+    //     context,
+    //     people,
+    //     content,
+    //     content_length,
+    //     content_tokens,
+    //     embedding,
+    //   })
+    //   .select('*');
+    // if (error) {
+    //   console.log('error', error);
+    // } else {
+    //   console.log('saved', i, i);
+    // }
+    // await new Promise((resolve) => setTimeout(resolve, 200));
   }
   return;
 };
@@ -114,88 +108,97 @@ const scrape = async (
   title: string,
   url: string
 ): Promise<Scraped | undefined> => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
-
-  await page.goto(loginUrl);
-
-  // Check if the button with the specified class exists and click it if found
-  const closeButtonSelector =
-    'CloseButton__ButtonElement-sc-79mh24-0 lnQjcm catoosa-CloseButton catoosa-close catoosa-ClosePosition--top-right';
-  const closeButton = await page.$(closeButtonSelector);
-
-  if (closeButton) {
-    await page.click(closeButtonSelector);
-  }
-
-  // Check if the button with the specified class exists and click it if found
-  const closeButtonSelector2 =
-    'CloseButton__ButtonElement-sc-79mh24-0 lnQjcm catoosa-CloseButton catoosa-close catoosa-ClosePosition--top-right';
-  const closeButton2 = await page.$(closeButtonSelector2);
-
-  if (closeButton2) {
-    await page.click(closeButtonSelector2);
-  }
-
-  await page.type('#user_login', username);
-  await page.type('#user_pass', password);
-  await page.click('#wp-submit');
-
-  await page.waitForTimeout(2000);
   await page.goto(url);
-  await page.waitForTimeout(2000);
 
-  let { content, category, date, firstH4 } = await page.evaluate(() => {
-    console.log('asd');
+  // Click the button with the specified classname to open the dialog
+  await page.click('.sc-31447687-0.kWYCdN');
 
-    const titleElement = document.querySelector(
-      '.heading--page'
-    ) as HTMLElement;
-    const categoryElement = document.querySelector(
-      '.list-item__category'
-    ) as HTMLElement;
-    const dateElement = document.querySelector(
-      '.list-item__meta'
-    ) as HTMLElement;
-    const contentElement = document.querySelector(
-      '.content--post'
-    ) as HTMLElement;
+  // Wait for the dialog elements to appear
+  await page.waitForSelector('.sc-28740013-0.gRpRjN');
+  await page.waitForSelector('.sc-9ee401a5-2.bTmyip');
+  await page.waitForSelector('.sc-1fe0cb69-0.gDRdqW');
 
-    const title = titleElement.textContent;
-    const category = categoryElement.textContent!;
-    const date = dateElement.textContent!;
-
-    const firstH4 = contentElement.querySelector('h4[id="notes"]:nth-child(1)');
-    const secondH4 = contentElement.querySelector(
-      'h4[id="notes"]:nth-child(2)'
-    );
-
-    console.log('ASDASDASDASDSAD');
-
-    const content =
-      firstH4 && secondH4
-        ? extractText(firstH4, secondH4).trim()
-        : contentElement.textContent;
-
-    return { content, category, date, firstH4 };
+  // Fetch the text content of the <div> element with the specified classname
+  const divText = await page.$eval('.sc-28740013-0.gRpRjN', (div: Element) => {
+    const divElement = div as HTMLDivElement;
+    return divElement.textContent;
   });
+
+  // Fetch the text content of the <table> element with the specified classname
+  const tableText = await page.$eval(
+    '.sc-9ee401a5-2.bTmyip',
+    (table: Element) => {
+      const tableElement = table as HTMLTableElement;
+      return tableElement.textContent;
+    }
+  );
+
+  // Fetch the text content of all the <a> elements with the specified classname
+  const aTexts = await page.$$eval(
+    '.sc-1fe0cb69-0.gDRdqW',
+    (aElements: Element[]) =>
+      aElements.map((a: Element) => {
+        const aElement = a as HTMLAnchorElement;
+        return aElement.textContent;
+      })
+  );
+
+  console.log('divText:', divText);
+  console.log('tableText:', tableText);
+  console.log('aTexts:', aTexts);
+
+  // Close the dialog
+  await page.click('sc-b8df2b91-0.ljUssp');
+
+  // Fetch the text content of all the <a> elements with the specified classname in the results function
+  const aTextsResults = await page.$$eval(
+    '.sc-1fe0cb69-0.gMWbKL.sc-4421a4fa-6.jNmnLj',
+    (aElements: Element[]) =>
+      aElements.map((a: Element) => {
+        const aElement = a as HTMLAnchorElement;
+        return aElement.textContent;
+      })
+  );
+
+  // Find all button elements with the specified classname
+  const scrapedData = await page.$$eval(
+    'button.sc-8c9b94e6-0.gXfAhD',
+    (buttons: Element[]) => {
+      return buttons.map((button: Element) => {
+        const h3Text = button.querySelector(
+          '.sc-5ba2664-2.gskVzg'
+        )?.textContent;
+        const pText1 = button.querySelector('.sc-5ba2664-0.ljehg')?.textContent;
+        const pText2 = button.querySelector(
+          '.sc-91d4e765-1.cURerU'
+        )?.textContent;
+        const imgElement = button.querySelector('img.sc-36a7e468-1.flwxdV');
+        const imgSrc = imgElement ? (imgElement as HTMLImageElement).src : null;
+        const popular = !!button.querySelector('span.sc-22183055-1.kNEbpF');
+
+        return {
+          h3Text,
+          pText1,
+          pText2,
+          imgSrc,
+          popular,
+        };
+      });
+    }
+  );
 
   await browser.close();
 
-  if (title && content) {
-    content = content.trim();
-    date = date.replace('\n', '').trim();
-    console.log('Title:', title.replace(/\s+/g, ' ').trim());
-    // console.log("Content:", content.trim());
-
-    const jsonContent = JSON.stringify({ title, content }, null, 2);
-    fs.writeFileSync(`scripts/data/raw/${title}.json`, jsonContent);
-    return { position, title, content, category, date };
-  } else {
-    console.log('Could not find the specified text or title.');
-    return undefined;
+  for (let i = 0; i < scrapedData.length; i++) {
+    const { h3Text, pText1, pText2 } = scrapedData[i];
+    console.log(h3Text, pText1, pText2);
   }
+
+  return undefined;
 };
+
 export const onParseFailure = async (failedContent: string) => {
   let response: any;
 
@@ -290,8 +293,8 @@ async function PP(content: any, tries?: number): Promise<any> {
   return parsed;
 }
 
-const parse = async (scraped: Scraped): Promise<Chunk[]> => {
-  const chunks: Chunk[] = [];
+const parse = async (scraped: Scraped): Promise<Restaurant[]> => {
+  const chunks: Restaurant[] = [];
 
   // const andyTry = andy.slice(17);
 
@@ -320,16 +323,16 @@ const parse = async (scraped: Scraped): Promise<Chunk[]> => {
         continue;
       }
 
-      chunks.push({
-        title: chunk.title,
-        date: scraped.date,
-        context: scraped.category,
-        people: 'Peter Attia',
-        content: chunk.content.trim(),
-        content_length: chunk.content.trim().length,
-        content_tokens: encode(chunk.content.trim()).length,
-        embedding: [],
-      });
+      // chunks.push({
+      //   title: chunk.name,
+      //   date: scraped.date,
+      //   context: scraped.category,
+      //   people: 'Peter Attia',
+      //   content: chunk.content.trim(),
+      //   content_length: chunk.content.trim().length,
+      //   content_tokens: encode(chunk.content.trim()).length,
+      //   embedding: [],
+      // });
     }
 
     console.log(content.length);
@@ -457,7 +460,7 @@ async function startScrape(results: any[]) {
 async function main() {
   const results: any[] = [];
 
-  fs.createReadStream('scripts/data/csv/heart_disease_unique.csv')
+  fs.createReadStream('scripts/data/wolt.csv')
     .pipe(csv())
     .on('data', (data: any) => results.push(data))
     .on('end', () => {
