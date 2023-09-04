9ef8fcf9f9822c7779989e85d17083760ede644a
Ulrik Håland <ulrik@netpower.no>
2023-09-04 22:03:58 +0200

    v2

--- next.config.js
+++ next.config.js
@@ -2,15 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    OPENAI_API_KEY: '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',
-    SUPABASE_URL: 'https://fwiyhnwcrlydinggswoe.supabase.co',
+    OPENAI_API_KEY: "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",
+    SUPABASE_URL: "https://fwiyhnwcrlydinggswoe.supabase.co",
     SUPABASE_SERVICE_ROLE_KEY:
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ3aXlobndjcmx5ZGluZ2dzd29lIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODIwOTIxNTksImV4cCI6MTk5NzY2ODE1OX0.LkbVrfkJe7yTxt0t6RS_ciiizXBSQ6JcNeYjhDdAjoE',
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpvbnJpZXZpcmh4ZmFod3hwa2ppIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM4NDg5ODYsImV4cCI6MjAwOTQyNDk4Nn0.F-3v4b4C7IS5RXxfRCjDBSvKzfpt4bzQDVStMqWTPLc",
     NEXT_PUBLIC_OPENAI_API_KEY:
-      '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',
-    NEXT_PUBLIC_SUPABASE_URL: 'https://fwiyhnwcrlydinggswoe.supabase.co',
+      "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",
+    NEXT_PUBLIC_SUPABASE_URL: "https://fwiyhnwcrlydinggswoe.supabase.co",
     NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY:
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ3aXlobndjcmx5ZGluZ2dzd29lIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODIwOTIxNTksImV4cCI6MTk5NzY2ODE1OX0.LkbVrfkJe7yTxt0t6RS_ciiizXBSQ6JcNeYjhDdAjoE',
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpvbnJpZXZpcmh4ZmFod3hwa2ppIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM4NDg5ODYsImV4cCI6MjAwOTQyNDk4Nn0.F-3v4b4C7IS5RXxfRCjDBSvKzfpt4bzQDVStMqWTPLc",
   },
 };
 
